d93cec3f364326d1a1002163c83f1c7a563d3f4c
bchavda288 <bharatchavda288@gmail.com>
2021-07-16 22:02:13 -0400

    addition

---
+++ Basic/4. Operators/AdditionInJava.java
@@ -0,0 +1,27 @@
+public class AdditionInJava{
+    public static void main(String[] args){
+
+        //Integer Addition
+        System.out.println("Adding Integers");
+        int a =5;
+        int b =12;
+        int c = a+b; //Addition of Integers
+        System.out.println("Adding a:"+a+" to b:"+b+" = "+c);
+
+        //Floating Numbers addition
+        float fa =4.34f;
+        float fb= 3.67f;
+        float fc = fa+fb;
+        System.out.println("="+fc);
+
+        //double  addition
+        double da = 5.34567;
+        double db = 32.34456788;
+        double dc= da+db;
+        System.out.println("="+dc);
+
+        
+
+
+    }
+}
